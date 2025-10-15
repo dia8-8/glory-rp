9e5b61ca2cb96c601534285e9c4d00e58b4a7ee9
diaa1408 <diaa1408@usherbrooke.ca>
2025-10-15 19:27:24 -0400

    streamers

--- data/streamers.ts
+++ data/streamers.ts
@@ -1,8 +1,9 @@
 export const STREAMERS = [
-  { name: 'M0nke', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=Fhlwy', link: 'https://kick.com' },
+  { name: 'Derrad', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=Fhlwy', link: 'https://kick.com' },
   { name: 'Eid Gaming', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=Drb7h', link: 'https://kick.com' },
   { name: 'Nefelibata', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=Peerless', link: 'https://kick.com' },
-  { name: 'xMusabRT', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=xMusabRT', link: 'https://kick.com' },
-  { name: 'S5B', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=S5B', link: 'https://kick.com' },
-
+  { name: 'Issa gaming', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=xMusabRT', link: 'https://kick.com' },
+  { name: 'SG Steve', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=S5B', link: 'https://kick.com' },
+  { name: 'Lolo', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=S5B', link: 'https://kick.com' },
+  { name: 'Al Prince', avatar: 'https://api.dicebear.com/9.x/shapes/svg?seed=S5B', link: 'https://kick.com' },
 ];
