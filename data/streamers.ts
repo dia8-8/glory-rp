27636d9b85416c7b2b9718f2cff6dc69748e3d6b
diaa1408 <diaa1408@usherbrooke.ca>
2025-10-29 21:47:21 -0400

    test images

--- data/streamers.ts
+++ data/streamers.ts
@@ -1,6 +1,6 @@
 export const STREAMERS = [
-  { name: 'Derrad', avatar: 'https://static-cdn.jtvnw.net/jtv_user_pictures/6cf8741b-f83f-4ce0-aa8e-383a9225d2f3-profile_image-70x70.png', link: 'https://www.twitch.tv/derrad_ggz' },
-  { name: 'Eid Gaming', avatar: 'https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/b92ba8126c866f2cd9dd6e1c364ba6d2~tplv-tiktokx-cropcenter:1080:1080.jpeg', link: 'https://www.tiktok.com/@eidgaming003id?lang=en' },
+  { name: 'Derrad', avatar: 'https://media.discordapp.net/attachments/1432229734611423303/1433261939886129313/Derrad_logo_1.png', link: 'https://www.twitch.tv/derrad_ggz' },
+  { name: 'Eid Gaming', avatar: 'https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/b92ba8126c866f2cd9dd6e1c364ba6d2~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=6a392565&x-expires=1761958800&x-signature=u%2FESIe6awHldBPWYOiQoiKpiXDo%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my', link: 'https://www.tiktok.com/@eidgaming003id?lang=en' },
   { name: 'Nefelibata', avatar: 'https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/6a6223ebbcbf22af57e19f72418c8260~tplv-tiktokx-cropcenter:1080:1080.jpeg', link: 'https://www.tiktok.com/@rison098?lang=en' },
   { name: 'Issa gaming', avatar: 'https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/96bd40d1db9f9d5542f14a9175986a72~tplv-tiktokx-cropcenter:100:100.jpeg', link: 'https://www.tiktok.com/@issagamingg?lang=en' },
   { name: 'Tripleh Gaming', avatar: 'https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/9659f9fe691366266e63ab493c8f3553~tplv-tiktokx-cropcenter:1080:1080.jpeg', link: 'https://www.tiktok.com/@triplehgaming?lang=en-GB'},
@@ -8,7 +8,7 @@ export const STREAMERS = [
   { name: 'Musquare', avatar: 'https://files.kick.com/images/user/49301460/profile_image/conversion/a54c1879-10c6-4e42-a466-00da4e02178e-fullsize.webp', link: 'https://kick.com/musquare' },
   { name: 'Ben 3adel', avatar: 'https://files.kick.com/images/user/13549654/profile_image/conversion/b375ebe3-2b6e-4ea8-baa9-4e1318c37a5b-fullsize.webp', link: 'https://kick.com/ben3adel' },
   { name: 'Mo3oss', avatar: 'https://files.kick.com/images/user/34839439/profile_image/conversion/a050c1cc-18c6-4812-bbb8-7462a9070714-fullsize.webp', link: 'https://kick.com/mo3oss' },
-  { name: '3awaddyy', avatar: 'https://files.kick.com/images/user/34839439/profile_image/conversion/a050c1cc-18c6-4812-bbb8-7462a9070714-fullsize.webp', link: 'https://kick.com/3awaddyy' },
+  { name: '3awaddyy', avatar: 'https://files.kick.com/images/user/62843837/profile_image/conversion/5ef95ee6-5acb-45c6-bd17-64a377542df5-fullsize.webp', link: 'https://kick.com/3awaddyy' },
   { name: 'Alfredo', avatar: 'https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/63233335bdf5115e501d49930422b0b1~tplv-tiktokx-cropcenter:1080:1080.jpeg', link: 'https://www.tiktok.com/@alfredo_ex?lang=en' },
   { name: 'Reboo0', avatar: 'https://static-cdn.jtvnw.net/jtv_user_pictures/4be7b628-5848-4292-90a3-3a9965446488-profile_image-70x70.png', link: 'https://www.twitch.tv/reboo0' },
 ];
